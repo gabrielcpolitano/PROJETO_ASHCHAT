02b709a8336b20ac35e3b3cac8260992872ead7e
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-06-07 13:16:51 -0300

    Thanks God

--- ES6/script.js
+++ ES6/script.js
@@ -1,125 +1,166 @@
-// 1 Diferença de variaveis em escopo em blocco
+// // 1 Diferença de variaveis em escopo em blocco
 
-// var
-var number = 1
+// // var
+// var number = 1
 
-console.log(number)
+// console.log(number)
 
-if(number === 1) {
-    var number = 5
-    console.log(number)
+// if(number === 1) {
+//     var number = 5
+//     console.log(number)
+// }
+
+// console.log(number)
+
+// // let
+// let numero = 2
+
+// console.log(numero)
+
+// if (numero === 2){
+//     let numero = 5
+//     console.log(numero)
+// }
+
+// console.log(numero)
+
+// // const
+// const num = 3
+
+// console.log(num)
+
+// if (num === 3){
+//     const num = 5
+//     console.log(num)
+// }
+
+// console.log(num)
+
+// // 2 arrow function
+// const sum = function sum(a, b) {
+//     return a + b
+// }
+
+// const arrowSum = (a, b) => a + b
+
+
+// console.log(sum(5, 5))
+// console.log(arrowSum(5, 5))
+
+// const greeting = (name) => {
+//     if(name) {
+//         return "Olá " + name + "!"
+//     } else {
+//         return "Olá!"
+//     }
+// }
+
+// console.log(greeting("Matheus"))
+// console.log(greeting())
+
+// const testeArrow = () => console.log("testou")
+
+// testeArrow()
+
+// const user = {
+//     name: 'Theo',
+//     sayUserName() {
+//         var self = this
+//         setTimeout(function (){
+//             console.log(self)
+//             console.log('Username: ' + self.name)
+//         }, 4000)
+//     },
+//     sayUserNameArrow() {
+//         setTimeout(() => {
+//             console.log(this)
+//             console.log('Username: ' + this.name)
+//         }, 4000)
+//     }
+// }
+
+// user.sayUserName()
+// user.sayUserNameArrow()
+
+// // 3 filter
+// const array = [1, 2, 3, 4, 5]
+
+// console.log(array)
+
+// const highNumbers = array.filter((n) => {
+//     if (n >= 3) {
+//         return n
+//     }
+// })
+
+// console.log(highNumbers)
+
+// const users = [
+//     {name: "Matheus", available: true},
+//     {name: "Pedro", available: false},
+//     {name: "Paulo", available: false},
+//     {name: "Miguel", available: true},
+// ]
+
+// const availableUsers = users.filter((user) => user.available)
+// const notAvailableUsers = users.filter((user) => !user.available)
+
+// console.log(availableUsers)
+// console.log(notAvailableUsers)
+
+// // 4 map
+// const products = [
+//     {name: 'Camisa', price: 10.99, category: 'Roupas'},
+//     {name: 'Chaleira Elétrica', price: 49.99, category: 'Eletro'},
+//     {name: 'Fogão',price: 400, category: 'Eletro'},
+//     {name: 'Calça jeans', price: 50.99, category: 'Roupas'},
+// ]
+
+// products.map((products) => {
+//     if (products.category === 'Roupas') {
+//         products.PROMOÇÃO = true
+//     }
+// })
+
+// console.log(products)
+
+// // 5 templates literals
+// const userName = 'Matheus'
+// const age = 30
+
+// console.log(`O nome do usuário é ${userName} e ele tem ${age} anos.`)
+
+// // 6 destructuring
+const fruit = ["Maça", "Laranja", "Mamão"]
+
+const [f1, f2, f3] = fruit
+
+console.log(f1)
+console.log(fruit[0])
+
+console.log(f2)
+console.log(fruit[1])
+
+console.log(f3)
+console.log(fruit[2])
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Tech",
+    color: "red"
 }
 
-console.log(number)
+const {name: productName, price: productPrice, category: productCategory, color: productColor} = productDetails
 
-// let
-let numero = 2
+console.log(productName)
+console.log(productDetails[productName])
 
-console.log(numero)
+console.log(productPrice)
+console.log(productDetails[1])
 
-if (numero === 2){
-    let numero = 5
-    console.log(numero)
-}
+console.log(productCategory)
+console.log(productDetails[2])
 
-console.log(numero)
-
-// const
-const num = 3
-
-console.log(num)
-
-if (num === 3){
-    const num = 5
-    console.log(num)
-}
-
-console.log(num)
-
-// 2 arrow function
-const sum = function sum(a, b) {
-    return a + b
-}
-
-const arrowSum = (a, b) => a + b
-
-
-console.log(sum(5, 5))
-console.log(arrowSum(5, 5))
-
-const greeting = (name) => {
-    if(name) {
-        return "Olá " + name + "!"
-    } else {
-        return "Olá!"
-    }
-}
-
-console.log(greeting("Matheus"))
-console.log(greeting())
-
-const testeArrow = () => console.log("testou")
-
-testeArrow()
-
-const user = {
-    name: 'Theo',
-    sayUserName() {
-        var self = this
-        setTimeout(function (){
-            console.log(self)
-            console.log('Username: ' + self.name)
-        }, 4000)
-    },
-    sayUserNameArrow() {
-        setTimeout(() => {
-            console.log(this)
-            console.log('Username: ' + this.name)
-        }, 4000)
-    }
-}
-
-user.sayUserName()
-user.sayUserNameArrow()
-
-// 3 filter
-const array = [1, 2, 3, 4, 5]
-
-console.log(array)
-
-const highNumbers = array.filter((n) => {
-    if (n >= 3) {
-        return n
-    }
-})
-
-console.log(highNumbers)
-
-const users = [
-    {name: "Matheus", available: true},
-    {name: "Pedro", available: false},
-    {name: "Paulo", available: false},
-    {name: "Miguel", available: true},
-]
-
-const availableUsers = users.filter((user) => user.available)
-const notAvailableUsers = users.filter((user) => !user.available)
-
-console.log(availableUsers)
-console.log(notAvailableUsers)
-
-// 4 map
-const products = [
-    {name: 'Camisa', price: 10.99, category: 'Roupas'},
-    {name: 'Chaleira Elétrica', price: 49.99, category: 'Eletro'},
-    {name: 'Fogão',price: 400, category: 'Eletro'},
-    {name: 'Calça jeans', price: 50.99, category: 'Roupas'},
-]
-
-products.map((products) => {
-    if (products.category === 'Roupas') {
-        products.PROMOÇÃO = true
-    }
-})
-
-console.log(products)
+console.log(productColor)
+console.log(productDetails[3])
