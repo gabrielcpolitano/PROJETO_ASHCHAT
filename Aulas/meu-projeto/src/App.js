c280004c27e2d3059221f100a4e9d901c2ab6f53
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-06-11 16:01:46 -0300

    Thanks God

---
+++ Aulas/meu-projeto/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+function App() {
+
+    const name = "Matheus"
+
+    const newName = name.toUpperCase()
+
+    function sum(a, b){
+        return a + b
+    }
+    const url = "https://img.clerk.com/static/apple.svg?width=80"
+  return (
+    <div className="App">
+      <HelloWorld/>
+      <SayMyName nome="Gabriel"/>
+      <SayMyName nome="Pedro"/>
+      <SayMyName nome={name}/>
+      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Aulas/meu-projeto/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return(
+        <div>
+            <p>Em nome do Senhor eu estudo programação paea ajudar pessoas e a minha familia</p>
+        </div>
+    )
+}
+
+export default Frase 
