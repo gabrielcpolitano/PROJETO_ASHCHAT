f3f811a88d70d60db95ae7bade84f2ad1b83bb7b
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-12-29 18:25:17 -0300

    Thanks God

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#19181f',   // Azul personalizado
+        secondary: '#F59E0B', // Amarelo personalizado
+        accent: '#10B981',    // Verde personalizado
+        customGray: '#6B7280', // Cinza personalizado
+        customDark: '#19181f',
+      },
+    },
+  },
+  plugins: [],
+};
